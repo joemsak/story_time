0bc62403268ac4bf5500b26f65f700d8c9a07734
Joe Sak <joe@joesak.com>
2015-01-19 10:01:54 -0500

    Move code so it loads properly

--- lib/story_time.rb
+++ lib/story_time.rb
@@ -4,8 +4,6 @@ require "story_time/time_tracker"
 require "story_time/story"
 
 module StoryTime
-  initialize_ticket_token
-
   def self.sync
     time.each do |time|
       story = TrackTimeOnStories.story(time)
@@ -16,17 +14,5 @@ module StoryTime
   private
   def self.time
     TrackTimeOnStories.time_referencing_stories
-  end
-
-  def self.time_tracker_client
-  end
-
-  def self.initialize_ticket_token
-    PivotalTracker::Client.token(ENV['PIVOTAL_USERNAME'],
-                                 ENV['PIVOTAL_PASSWORD'])
-  end
-
-  def self.ticket_tracker_story
-    PivotalTracker::Story
   end
 end
--- lib/story_time/story.rb
+++ lib/story_time/story.rb
@@ -1,5 +1,8 @@
 require 'pivotal-tracker'
 
+PivotalTracker::Client.token(ENV['PIVOTAL_USERNAME'],
+                             ENV['PIVOTAL_PASSWORD'])
+
 module StoryTime
   Story =  PivotalTracker::Story
 end
